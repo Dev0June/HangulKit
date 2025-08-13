5bfb3c157f341dfda74601da80d02896e1e0b755
jbk <detox218@gmail.com>
2025-08-13 10:44:40 +0900

    영문을 위한 wrapper 정리

--- Sources/EngWrapper.h
+++ Sources/EngWrapper.h
@@ -15,64 +15,27 @@ NS_ASSUME_NONNULL_BEGIN
 
 // Constants
 typedef NS_ENUM(NSInteger, EngKeyboardType) {
-    EngKeyboardTypeHalfStandard = 0,
+    EngKeyboardTypeHalfQwertyWide = 0,
     EngKeyboardTypeHalfQwertyLeft = 1,
     EngKeyboardTypeHalfQwertyRight = 2
 };
 
-// Typing statistics
-@interface EngTypingStats : NSObject
-@property (nonatomic, assign) int totalChars;
-@property (nonatomic, assign) int mirrorChars;
-@property (nonatomic, assign) int errors;
-@property (nonatomic, assign) long startTimeMs;
-@property (nonatomic, assign) long endTimeMs;
-@property (nonatomic, assign) double wpm;
-@property (nonatomic, assign) double accuracy;
-@end
-
 // English Input Context
 @interface EngInputContext : NSObject
-
 - (instancetype)initWithKeyboardType:(EngKeyboardType)type;
-
-// Convenience initializers
-+ (instancetype)newHalfQwertyLeft;
-+ (instancetype)newHalfQwertyRight;
 
 // Input processing
 - (BOOL)processKey:(int)ascii;
 - (BOOL)processKeyDown:(int)ascii;
 - (BOOL)processKeyUp:(int)ascii;
-- (void)reset;
 
 // Space key handling
-- (void)setSpaceDown:(BOOL)down;
 - (BOOL)isSpaceDown;
-
-// State queries
-- (BOOL)isEmpty;
-
-// Settings
-- (void)setKeyboardType:(EngKeyboardType)type;
-- (EngKeyboardType)keyboardType;
+- (BOOL)isSpaceUsed;
+- (void)resetSpaceState;
 
 // Timeout settings
 - (void)setSpaceTimeout:(int)timeoutMs;
-- (int)spaceTimeout;
-
-// Sticky Keys support
-- (void)setStickyKeys:(BOOL)enabled;
-- (BOOL)stickyKeys;
-- (void)setShiftSticky:(BOOL)sticky;
-- (void)setCtrlSticky:(BOOL)sticky;
-- (void)setAltSticky:(BOOL)sticky;
-
-// Typing statistics
-- (void)startTypingTest;
-- (void)endTypingTest;
-- (EngTypingStats *)typingStats;
-- (void)resetTypingStats;
 
 // Output
 - (NSString *)preeditString;
--- Sources/EngWrapper.m
+++ Sources/EngWrapper.m
@@ -11,17 +11,11 @@
 // C 라이브러리 타입들을 다른 이름으로 정의하여 충돌 방지
 #define EngInputContext CEngInputContext
 #define EngKeyboardType CEngKeyboardType
-#define EngTypingStats CEngTypingStats
 
 #include "enginputcontext.h"
 
 #undef EngInputContext
 #undef EngKeyboardType
-#undef EngTypingStats
-
-// EngTypingStats implementation
-@implementation EngTypingStats
-@end
 
 @interface EngInputContext() {
     CEngInputContext* _context;
@@ -29,14 +23,13 @@
 @end
 
 @implementation EngInputContext
-
 - (instancetype)initWithKeyboardType:(EngKeyboardType)type {
     self = [super init];
     if (self) {
         CEngKeyboardType cType;
         switch (type) {
-            case EngKeyboardTypeHalfStandard:
-                cType = ENG_KEYBOARD_TYPE_HALF_STANDARD;
+            case EngKeyboardTypeHalfQwertyWide:
+                cType = ENG_KEYBOARD_TYPE_HALF_QWERTY_WIDE;
                 break;
             case EngKeyboardTypeHalfQwertyLeft:
                 cType = ENG_KEYBOARD_TYPE_HALF_QWERTY_LEFT;
@@ -45,21 +38,12 @@
                 cType = ENG_KEYBOARD_TYPE_HALF_QWERTY_RIGHT;
                 break;
             default:
-                cType = ENG_KEYBOARD_TYPE_HALF_QWERTY_LEFT; // 왼손을 기본으로 설정
+                cType = ENG_KEYBOARD_TYPE_HALF_QWERTY_WIDE; // QWERTY WIDE를 기본으로 설정
                 break;
         }
         _context = eng_ic_new(cType);
     }
     return self;
-}
-
-// Convenience initializers
-+ (instancetype)newHalfQwertyLeft {
-    return [[self alloc] initWithKeyboardType:EngKeyboardTypeHalfQwertyLeft];
-}
-
-+ (instancetype)newHalfQwertyRight {
-    return [[self alloc] initWithKeyboardType:EngKeyboardTypeHalfQwertyRight];
 }
 
 - (void)dealloc {
@@ -85,141 +69,26 @@
     return eng_ic_process_key_up(_context, ascii);
 }
 
-- (void)reset {
-    if (_context) {
-        eng_ic_reset(_context);
-    }
-}
-
 // Space key handling
-- (void)setSpaceDown:(BOOL)down {
-    if (_context) {
-        eng_ic_set_space_down(_context, down);
-    }
-}
-
 - (BOOL)isSpaceDown {
     if (!_context) return NO;
     return eng_ic_is_space_down(_context);
 }
 
-// State queries
-- (BOOL)isEmpty {
-    if (!_context) return YES;
-    return eng_ic_is_empty(_context);
+- (BOOL)isSpaceUsed {
+    if (!_context) return NO;
+    return eng_ic_is_space_used(_context);
 }
 
-// Settings
-- (void)setKeyboardType:(EngKeyboardType)type {
+- (void)resetSpaceState {
     if (!_context) return;
-    
-    CEngKeyboardType cType;
-    switch (type) {
-        case EngKeyboardTypeHalfStandard:
-            cType = ENG_KEYBOARD_TYPE_HALF_STANDARD;
-            break;
-        case EngKeyboardTypeHalfQwertyLeft:
-            cType = ENG_KEYBOARD_TYPE_HALF_QWERTY_LEFT;
-            break;
-        case EngKeyboardTypeHalfQwertyRight:
-            cType = ENG_KEYBOARD_TYPE_HALF_QWERTY_RIGHT;
-            break;
-        default:
-            cType = ENG_KEYBOARD_TYPE_HALF_QWERTY_LEFT;
-            break;
-    }
-    eng_ic_set_keyboard_type(_context, cType);
-}
-
-- (EngKeyboardType)keyboardType {
-    if (!_context) return EngKeyboardTypeHalfQwertyLeft;
-    
-    CEngKeyboardType cType = eng_ic_get_keyboard_type(_context);
-    switch (cType) {
-        case ENG_KEYBOARD_TYPE_HALF_STANDARD:
-            return EngKeyboardTypeHalfStandard;
-        case ENG_KEYBOARD_TYPE_HALF_QWERTY_LEFT:
-            return EngKeyboardTypeHalfQwertyLeft;
-        case ENG_KEYBOARD_TYPE_HALF_QWERTY_RIGHT:
-            return EngKeyboardTypeHalfQwertyRight;
-        default:
-            return EngKeyboardTypeHalfQwertyLeft;
-    }
+    eng_ic_reset_space_state(_context);
 }
 
 // Timeout settings
 - (void)setSpaceTimeout:(int)timeoutMs {
     if (_context) {
         eng_ic_set_space_timeout(_context, timeoutMs);
-    }
-}
-
-- (int)spaceTimeout {
-    if (!_context) return 0;
-    return eng_ic_get_space_timeout(_context);
-}
-
-// Sticky Keys support
-- (void)setStickyKeys:(BOOL)enabled {
-    if (_context) {
-        eng_ic_set_sticky_keys(_context, enabled);
-    }
-}
-
-- (BOOL)stickyKeys {
-    if (!_context) return NO;
-    return eng_ic_get_sticky_keys(_context);
-}
-
-- (void)setShiftSticky:(BOOL)sticky {
-    if (_context) {
-        eng_ic_set_shift_sticky(_context, sticky);
-    }
-}
-
-- (void)setCtrlSticky:(BOOL)sticky {
-    if (_context) {
-        eng_ic_set_ctrl_sticky(_context, sticky);
-    }
-}
-
-- (void)setAltSticky:(BOOL)sticky {
-    if (_context) {
-        eng_ic_set_alt_sticky(_context, sticky);
-    }
-}
-
-// Typing statistics
-- (void)startTypingTest {
-    if (_context) {
-        eng_ic_start_typing_test(_context);
-    }
-}
-
-- (void)endTypingTest {
-    if (_context) {
-        eng_ic_end_typing_test(_context);
-    }
-}
-
-- (EngTypingStats *)typingStats {
-    if (!_context) return nil;
-    
-    CEngTypingStats cStats = eng_ic_get_typing_stats(_context);
-    EngTypingStats *stats = [[EngTypingStats alloc] init];
-    stats.totalChars = cStats.total_chars;
-    stats.mirrorChars = cStats.mirror_chars;
-    stats.errors = cStats.errors;
-    stats.startTimeMs = cStats.start_time_ms;
-    stats.endTimeMs = cStats.end_time_ms;
-    stats.wpm = cStats.wpm;
-    stats.accuracy = cStats.accuracy;
-    return [stats autorelease];
-}
-
-- (void)resetTypingStats {
-    if (_context) {
-        eng_ic_reset_typing_stats(_context);
     }
 }
 
@@ -251,7 +120,8 @@
     const char* commit = eng_ic_get_commit_string(_context);
     if (commit) {
         NSString *result = [NSString stringWithUTF8String:commit];
-        eng_ic_reset(_context);
+        // Space 상태만 리셋 (영어 입력에서는 전체 리셋이 불필요)
+        eng_ic_reset_space_state(_context);
         return result;
     }
     return @"";

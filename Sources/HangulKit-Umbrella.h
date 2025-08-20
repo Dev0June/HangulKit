9a38c1734f0c34b28640385645a03eaa3847d57f
jbk <detox218@gmail.com>
2025-08-21 01:31:33 +0900

    구름입력기 xcode에서 빌드하는 방식을 따라 했는데. 원래는 스크립트를 짜서 하다 너무 복잡해져 바꿈

---
+++ Sources/HangulKit-Umbrella.h
@@ -0,0 +1,19 @@
+//
+//  HangulKit-Umbrella.h
+//  HangulKit
+//
+//  Umbrella header for HangulKit framework
+//  Based on libhangul Korean input library
+//
+
+@import Foundation;
+
+//! Project version number for HangulKit.
+FOUNDATION_EXPORT double HangulKitVersionNumber;
+
+//! Project version string for HangulKit.
+FOUNDATION_EXPORT const unsigned char HangulKitVersionString[];
+
+// Public headers of the framework
+#import <HangulKit/HangulWrapper.h>
+#import <HangulKit/hangul.h>
